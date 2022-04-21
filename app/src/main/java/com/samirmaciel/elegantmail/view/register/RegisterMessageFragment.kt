26956aee7d854d4bba3e024811258f374bdaf661
Samir Maciel <samirmaciel@mc1global.com>
2022-04-20 21:55:15 -0300

    create fragments

---
+++ app/src/main/java/com/samirmaciel/elegantmail/view/register/RegisterMessageFragment.kt
@@ -0,0 +1,23 @@
+package com.samirmaciel.elegantmail.view.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.samirmaciel.elegantmail.R
+
+
+class RegisterMessageFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register_message, container, false)
+    }
+
+
+}
